6a4ca7a323cba2e5a4076de9334980f1ca49ab90
Robert J. Paruch <roobbb@live.com>
2019-06-24 04:57:45 -0700

    Added Navbar

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { LinkContainer } from 'react-router-bootstrap';
+
+export default function Navbar2() {
+  return (
+    <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+      <Navbar.Brand href="/search">Google Books</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <LinkContainer to="/search">
+            <Nav.Link>Search for Books</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/saved">
+            <Nav.Link>Your Saved Books</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
