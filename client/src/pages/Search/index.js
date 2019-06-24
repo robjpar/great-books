4e7b710055b1ee49e6f0af8731e41b80ca371e3d
Robert J. Paruch <roobbb@live.com>
2019-06-24 04:57:58 -0700

    Added pages

---
+++ client/src/pages/Search/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+
+export default class Search extends Component {
+  state = {};
+
+  render() {
+    return (
+      <Container>
+        <div> search </div>
+      </Container>
+    );
+  }
+}
