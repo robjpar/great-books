daa0a14f11cc12fc3500a003e6e3762495e9d5e1
Robert J. Paruch <roobbb@live.com>
2019-06-24 04:57:21 -0700

    Added code for App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Search from './pages/Search';
+import Saved from './pages/Saved';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/saved" component={Saved} />
+        <Route component={Search} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
